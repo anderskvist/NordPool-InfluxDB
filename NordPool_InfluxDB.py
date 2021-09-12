57de5453256aa5f21c217b923c37b5e8937c8038
flopp999 <21694965+flopp999@users.noreply.github.com>
2021-09-12 08:43:06 +0200

    Update NordPool_InfluxDB.py

--- NordPool_InfluxDB.py
+++ NordPool_InfluxDB.py
@@ -1,13 +1,10 @@
 # Created by Daniel Nilsson
 # Please support me with a coffee https://www.buymeacoffee.com/flopp999
-# Version 1.0
+# Version 1.1
 # Script to fetch hourly prices from Nord Pool and store the data into InfluxDB
 from nordpool import elspot
 from datetime import datetime
 from influxdb import InfluxDBClient, exceptions
-
-prices_spot = elspot.Prices("SEK")
-price=prices_spot.hourly(end_date=datetime.now().date(),areas=["SE4"])
 
 # InfluxDB variables
 database = "Tibber"
@@ -18,14 +15,28 @@ field = "Pris"
 client = InfluxDBClient('localhost', 8086) # IP, port, user, password
 
 jsondata = []
+#Today
 hour = 0
+prices_spot = elspot.Prices("SEK")
+price=prices_spot.hourly(end_date=datetime.now().date(),areas=["SE4"])
 for each,b in price["areas"]["SE4"].items():
     if each == "values":
         for each in price["areas"]["SE4"]["values"]:
             time = int(each["start"].timestamp()) # store the datetime
             hour += 1
-            jsondata.append({"measurement": measurement, "time": time, "fields": {field: float(each["value"]/10.0)}})
-
+            jsondata.append({"measurement": measurement, "time": time, "fields": {field: float(round(each["value"]/10.0,1))}})
+#Tomorrow
+hour = 0
+price=prices_spot.hourly(areas=["SE4"])
+for each,b in price["areas"]["SE4"].items():
+    if each == "values":
+        for each in price["areas"]["SE4"]["values"]:
+            if str(each["value"]) == "inf":
+                continue
+            time = int(each["start"].timestamp()) # store the datetime
+            hour += 1
+            jsondata.append({"measurement": measurement, "time": time, "fields": {field: float(round(each["value"]/10.0,1))}})
+#Store to InfluxDB
 try:
     client.write_points(jsondata, database=database, time_precision='s', batch_size=10000, protocol='json')  # skriver data till Influx
 except exceptions.InfluxDBClientError:
